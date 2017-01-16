67732320e9679ecce881bafbd302f781e9bb979c
amelieykw <amelieykw1991@gmail.com>
2017-01-17 00:02:40 +0100

    can change the value of web components by the message sent by real device

--- webaudio.js
+++ webaudio.js
@@ -16,6 +16,9 @@ for (var i = 0; i < switchs.length; i++) {
     var switcher = switchs[i];
     switcher.addEventListener('change', change);
 }
+// params
+var params = document.getElementsByTagName('webaudio-param');
+var paramsVal = [0, 0];
 // keys
 //var keyboard = document.getElementById("keyboard");
 //keyboard.addEventListener('change', changeKeyboard);
@@ -96,8 +99,75 @@ function listInputsAndOutputs( midiAccess ) {
 function onMIDIMessage( event ) {
     var str = "MIDI message received [" + event.data.length + " bytes]: ";
     for (var i=0; i<event.data.length; i++) {
-        str += event.data[i].toString(10) + " ";
+        str += "(" + i + ")"+ event.data[i].toString(10) + " "; 
+        
+        // change the value of web components by the message sent by real device
+        var event_data_1 = event.data[1].toString(10);
+        if (event_data_1 == 21) {
+            knobs[0].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 22) {
+            knobs[1].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 23) {
+            knobs[2].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 24) {
+            knobs[3].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 25) {
+            knobs[4].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 26) {
+            knobs[5].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 27) {
+            knobs[6].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 28) {
+            knobs[7].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 41) {
+            knobs[8].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 42) {
+            knobs[9].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 43) {
+            knobs[10].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 44) {
+            knobs[11].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 45) {
+            knobs[12].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 46) {
+            knobs[13].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 47) {
+            knobs[14].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 48) {
+            knobs[15].setValue(event.data[2].toString(10), true);
+        } else if (event_data_1 == 114) {
+            if (event.data[2].toString(10) == 127) {
+                sliders[0].setValue(++ paramsVal[0], true);
+            }
+        } else if (event_data_1 == 115) {
+            if (event.data[2].toString(10) == 127) {
+                sliders[0].setValue(-- paramsVal[0], true);
+            }
+        } else if (event_data_1 == 116) {
+            if (event.data[2].toString(10) == 127) {
+                sliders[1].setValue(++ paramsVal[1], true);
+            }
+        } else if (event_data_1 == 117) {
+            if (event.data[2].toString(10) == 127) {
+                sliders[1].setValue(-- paramsVal[1], true);
+            }
+        } else if (event_data_1 == 0) {
+            if (event.data[7].toString(10) == 0) {
+                switchs[0].setValue(1, true);
+                switchs[1].setValue(0, true);
+            } else if (event.data[7].toString(10) == 8){
+                switchs[0].setValue(0, true);
+                switchs[1].setValue(1, true);
+            } else {
+                switchs[0].setValue(0, true);
+                switchs[1].setValue(0, true);
+            }
+        } else {
+            
+        }
+        
     }
+    
     console.log( str );
 }
 
